44ffc4dc5c66f915acc80fa85c9af9141a84e362
tsukuneA1 <tkschr1125@gmail.com>
2025-11-25 00:06:27 +0900

    fix

--- 2nd/ST.c
+++ 2nd/ST.c
@@ -1,9 +1,11 @@
+// C言語の標準ライブラリ、malloc関数を呼び出す
 #include <stdlib.h>
 #include "Item.h"
 typedef struct STnode* link;
+// Nはノード以下の項目数
 struct STnode { Item item; link l, r; int N; };
+// headは木の根、zは葉を表す。番兵的な役割を果たす
 static link head, z;
-
 link NEW(Item item, link l, link r, int N)
 {
     link x = malloc(sizeof *x);
@@ -18,13 +20,17 @@ void STinit()
 {
     head = (z = NEW(NULLitem, 0, 0, 0));
 }
-int STcount(void) { return head->N; }
+
+int STcount(void)
+{
+    return head->N;
+}
 
 Item searchR(link h, Key v)
 {
     Key t = key(h->item);
     if (h == z) return NULLitem;
-    if (eq(v, t)) return h->item;
+    if eq(v, t) return h->item;
     if less(v, t) return searchR(h->l, v);
     else return searchR(h->r, v);
 }
@@ -39,10 +45,9 @@ link insertR(link h, Item item)
     Key v = key(item);
     Key t = key(h->item);
     if (h == z) return NEW(item, z, z, 1);
-    if (less(v, t))
+    if less(v, t)
         h->l = insertR(h->l, item);
-    else
-        h->r = insertR(h->r, item);
+    else h->r = insertR(h->r, item);
     (h->N)++; return h;
 }
 
